4ca822feda200a1ba0d52877cc698568eb3ac734
Jason Winnebeck <gillius@gillius.org>
2015-10-22 23:28:35 -0400

    Allow setting a custom tooltip function
    Allow true/false and also custom function to be passed to tooltip
    property, to implement spiermar/d3-flame-graph#32

--- src/d3.flameGraph.js
+++ src/d3.flameGraph.js
@@ -25,9 +25,17 @@
       document.getElementById("details").innerHTML = t;
     }
 
+    function defaultLabel(d) {
+      return d.name + " (" + d3.round(100 * d.dx, 3) + "%, " + d.value + " samples)";
+    }
+
     function label(d) {
       if (!d.dummy) {
-        return d.name + " (" + d3.round(100 * d.dx, 3) + "%, " + d.value + " samples)";
+        if (typeof tooltip === "function") {
+          return tooltip(d);
+        } else if (tooltip) {
+          return defaultLabel(d);
+        }
       } else {
         return "";
       }
