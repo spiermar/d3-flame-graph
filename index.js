80cdce8c9a76f40b894aa663300e947c1c3e6991
Martin Spier <spiermar@gmail.com>
2020-03-19 18:25:55 -0700

    fix: merge issue

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export { default as flamegraph } from './dist/d3-flamegraph.js'
 export { offCpuColorMapper, allocationColorMapper, nodeJsColorMapper } from './dist/d3-flamegraph-colorMapper.js'
-export { flamegraphTooltip } from './dist/d3-flamegraph-tooltip.js'
+export { defaultFlamegraphTooltip } from './dist/d3-flamegraph-tooltip.js'
