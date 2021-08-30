9c61d628fa3b68853149aab48638e20cdc009f65
Andreas Gerstmayr <agerstmayr@redhat.com>
2021-08-30 14:58:45 +0200

    chore: extract duplicated code into separate files

--- src/colorMapper.js
+++ src/colorMapper.js
@@ -1,3 +1,5 @@
+import { generateColorVector } from './colorUtils'
+
 function pickHex (color1, color2, weight) {
     const w1 = weight
     const w2 = 1 - w1
@@ -5,30 +7,6 @@ function pickHex (color1, color2, weight) {
         Math.round(color1[1] * w1 + color2[1] * w2),
         Math.round(color1[2] * w1 + color2[2] * w2)]
     return rgb
-}
-
-function generateHash (name) {
-    // Return a vector (0.0->1.0) that is a hash of the input string.
-    // The hash is computed to favor early characters over later ones, so
-    // that strings with similar starts have similar vectors. Only the first
-    // 6 characters are considered.
-    const MAX_CHAR = 6
-
-    var hash = 0
-    var maxHash = 0
-    var weight = 1
-    var mod = 10
-
-    if (name) {
-        for (var i = 0; i < name.length; i++) {
-            if (i > MAX_CHAR) { break }
-            hash += weight * (name.charCodeAt(i) % mod)
-            maxHash += weight * (mod - 1)
-            weight *= 0.70
-        }
-        if (maxHash > 0) { hash = hash / maxHash }
-    }
-    return hash
 }
 
 export function allocationColorMapper (d, originalColor) {
@@ -44,15 +22,8 @@ export function allocationColorMapper (d, originalColor) {
 export function offCpuColorMapper (d, originalColor) {
     if (d.highlight) return originalColor
 
-    let name = d.data.n || d.data.name
-    let vector = 0
-    const nameArr = name.split('`')
-
-    if (nameArr.length > 1) {
-        name = nameArr[nameArr.length - 1] // drop module name if present
-    }
-    name = name.split('(')[0] // drop extra info
-    vector = generateHash(name)
+    const name = d.data.n || d.data.name
+    const vector = generateColorVector(name)
 
     const r = 0 + Math.round(55 * (1 - vector))
     const g = 0 + Math.round(230 * (1 - vector))
---
+++ src/colorScheme.js
@@ -0,0 +1,48 @@
+export function calculateColor (hue, vector) {
+    var r
+    var g
+    var b
+
+    if (hue === 'red') {
+        r = 200 + Math.round(55 * vector)
+        g = 50 + Math.round(80 * vector)
+        b = g
+    } else if (hue === 'orange') {
+        r = 190 + Math.round(65 * vector)
+        g = 90 + Math.round(65 * vector)
+        b = 0
+    } else if (hue === 'yellow') {
+        r = 175 + Math.round(55 * vector)
+        g = r
+        b = 50 + Math.round(20 * vector)
+    } else if (hue === 'green') {
+        r = 50 + Math.round(60 * vector)
+        g = 200 + Math.round(55 * vector)
+        b = r
+    } else if (hue === 'pastelgreen') {
+        // rgb(163,195,72) - rgb(238,244,221)
+        r = 163 + Math.round(75 * vector)
+        g = 195 + Math.round(49 * vector)
+        b = 72 + Math.round(149 * vector)
+    } else if (hue === 'blue') {
+        // rgb(91,156,221) - rgb(217,232,247)
+        r = 91 + Math.round(126 * vector)
+        g = 156 + Math.round(76 * vector)
+        b = 221 + Math.round(26 * vector)
+    } else if (hue === 'aqua') {
+        r = 50 + Math.round(60 * vector)
+        g = 165 + Math.round(55 * vector)
+        b = g
+    } else if (hue === 'cold') {
+        r = 0 + Math.round(55 * (1 - vector))
+        g = 0 + Math.round(230 * (1 - vector))
+        b = 200 + Math.round(55 * vector)
+    } else {
+        // original warm palette
+        r = 200 + Math.round(55 * vector)
+        g = 0 + Math.round(230 * (1 - vector))
+        b = 0 + Math.round(55 * (1 - vector))
+    }
+
+    return 'rgb(' + r + ',' + g + ',' + b + ')'
+}
