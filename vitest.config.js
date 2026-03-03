91afa0dfaf8b0c04e4918393d077d8f16a73debb
Martin Spier <spiermar@gmail.com>
2026-03-03 08:48:12 -0600

    migrate from jest to vitest

---
+++ vitest.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+    test: {
+        environment: "jsdom",
+        include: ["test/**/*.js"],
+        globals: true,
+        environmentOptions: {
+            jsdom: {
+                pretendToBeVisual: true,
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "d3-flamegraph": resolve(__dirname, "src/flamegraph.js"),
+        },
+    },
+});
