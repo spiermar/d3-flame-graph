bca0d09e0312b73e95504e120a36ad798cdd1167
Martin Spier <spiermar@gmail.com>
2026-03-03 08:41:23 -0600

    refactor: replace babel with esbuild-jest for tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -180,7 +180,7 @@ module.exports = {
     // A map from regular expressions to paths to transformers
     // transform: undefined,
     transform: {
-        "^.+\\.[t|j]sx?$": "babel-jest",
+        "^.+\\.[t|j]sx?$": "esbuild-jest",
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
