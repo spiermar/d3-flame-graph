05f10449abef320ba088b1fa01e805a8e36c1dd8
Martin Spier <spiermar@gmail.com>
2026-03-04 00:46:00 -0600

    feat: add color mapper example config

---
+++ src/examples/colorMapper.js
@@ -0,0 +1,36 @@
+import { flamegraph } from "../lib";
+import { setupFormHandlers } from "../lib/common.js";
+
+export function createColorMapperChart() {
+    var chart = flamegraph()
+        .width(960)
+        .cellHeight(18)
+        .transitionDuration(750)
+        .minFrameSize(5)
+        .transitionEase(d3.easeCubic)
+        .sort(true)
+        .title("")
+        .onClick(onClick)
+        .selfValue(false)
+        .setColorMapper(flamegraph.colorMapper.offCpuColorMapper);
+
+    var details = document.getElementById("details");
+    chart.setDetailsElement(details);
+
+    setupFormHandlers(chart);
+
+    d3.json("stacks.json")
+        .then((data) => {
+            d3.select("#chart").datum(data).call(chart);
+        })
+        .catch((error) => {
+            return console.warn(error);
+        });
+
+    return chart;
+}
+
+function onClick(d) {
+    console.info(`Clicked on ${d.data.name}, id: "${d.id}"`);
+    history.pushState({ id: d.id }, d.data.name, `#${d.id}`);
+}
